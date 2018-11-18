4c18031bebc14dc3fecbc66fe993654aa338d35d
crea <masataka0504@gmail.com>
2018-11-18 14:47:09 +0900

    Initial Commit

---
+++ KentBeckTDD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KentBeckTDD
+//
+//  Created by crea on 2018/11/18.
+//  Copyright © 2018 crea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
